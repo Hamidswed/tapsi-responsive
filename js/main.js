c333f61bcf48c20c01c3f71acf8f66e632e3dab5
HamidReza <delshad.swdn@gmail.com>
2024-04-02 19:43:25 +0330

    add services and its functions

--- js/main.js
+++ js/main.js
@@ -1,6 +1,22 @@
-const toggler = document.querySelector(".nav__toggler");
-const navbar = document.querySelector(".nav");
+// const toggler = document.querySelector(".nav__toggler");
+// const navbar = document.querySelector(".nav");
 
-toggler.addEventListener("click", (e) => {
-  navbar.classList.toggle("nav__expanded");
+// toggler.addEventListener("click", (e) => {
+//   navbar.classList.toggle("nav__expanded");
+// });
+
+const tabs = document.querySelectorAll(".tab");
+const tabContents = document.querySelectorAll(".tab-content");
+tabs.forEach((tab) => {
+  tab.addEventListener("click", (e) => {
+    const targetTabContent = document.querySelector(
+      `#${tab.dataset.tabTarget}`
+    );
+
+    tabs.forEach((tab) => tab.classList.remove("active"));
+    tabContents.forEach((tabContent) => tabContent.classList.remove("active"));
+
+    tab.classList.add("active");
+    targetTabContent.classList.add("active");
+  });
 });
